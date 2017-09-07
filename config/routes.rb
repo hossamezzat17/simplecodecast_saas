1331d53cacd8ae358abcfb5de76325bbcb69a9d7
Hossam Ezzat <hossam.ezzat99@gmail.com>
2017-09-07 13:21:56 +0000

    Improved the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    get '/about' => 'pages#about'
+    get '/aboutme' => 'pages#aboutme'
+    get '/aboutmycompany' => 'pages#aboutmycompany'
+    get '/aboutmyproducts' => 'pages#aboutmyproducts'
     root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
