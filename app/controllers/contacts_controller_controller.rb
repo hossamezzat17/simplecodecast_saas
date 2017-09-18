2689719de5b0a56ebd268dd6c04d1f60a78aa4ae
Hossam Ezzat <hossam.ezzat99@gmail.com>
2017-09-18 11:17:47 +0000

    Created contact form view

--- app/controllers/contacts_controller_controller.rb
+++
@@ -1,8 +0,0 @@
-class ContactsControllerController < ApplicationController
-    def new
-    end
-
-    def create
-    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     resources :contacts
-    get '/store' => 'pages#Store'
     get '/about' => 'pages#About'
     root 'pages#Home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
